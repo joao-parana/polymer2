0323b8a0f65991909f03828e1fd7f73f226b11b4
Joao Antonio Ferreira <joao.parana@gmail.com>
2017-03-25 13:23:52 -0300

    Acertando editable-name-tag e o teste em testENT.html

--- es6/app.js
+++ es6/app.js
@@ -1,3 +1,5 @@
-let name = "João"
-const age = (year) => new Date().getFullYear() - year
-console.log(`DEBUG: name = ${name}, age ${age(1965)}` )
+(function(){
+  let name = "João";
+  const age = (year) => new Date().getFullYear() - year;
+  console.log(`DEBUG: name = ${name}, age ${age(1965)}`)
+})();
